fed805db27d7173dca07c1cf0af5fcd1c5e5e4ce
samas3 <samas3@163.com>
2024-08-28 19:10:04 +0800

    fix

---
+++ chart.py
@@ -0,0 +1,123 @@
+import json
+import line
+import util
+import pygame
+import judge
+class Chart():
+    def __init__(self, chart, info={}, options={}):
+        chart = json.load(open(chart))
+        self.options = options
+        self.fv = int(chart['formatVersion'])
+        self.offset = float(chart['offset'])
+        self.lines = []
+        self.all_notes = []
+        for i, j in enumerate(chart['judgeLineList']):
+            self.lines.append(line.Line(j, i))
+        for i in self.lines:
+            i = number_notes(i)
+            self.all_notes += i.notesAbove + i.notesBelow
+            for j, k in enumerate(i.speedEvents):
+                if j == 0:
+                    i.speedEvents[j].floorPosition = 0
+                else:
+                    i.speedEvents[j].floorPosition = i.speedEvents[j - 1].floorPosition + i.speedEvents[j - 1].value * util.timeToSec(i.bpm, k.startTime - i.speedEvents[j - 1].startTime)
+        self.all_notes.sort(key=lambda x: x.time + x.holdTime)
+        self.jm = judge.JudgeManager(self.all_notes)
+        self.check_line()
+        self.highlight()
+        self.name = info.get('name', 'Untitled')
+        self.lvl = info.get('lvl', 'SP Lv.?')
+        self.numOfNotes = self.notes()
+    def print(self, str):
+        if 'printlog' in self.options:
+            print(str)
+    def check_line_event(self, line, evt, isSpeed=False):
+        flag = 1
+        for i, j in enumerate(evt):
+            if i == 0 and j.startTime >= 0 and not isSpeed:
+                self.print(f'Warning: JudgeLine{line.id}.Event[0].startTime >= 0')
+                flag = 0
+            elif i == 0 and j.startTime != 0 and isSpeed:
+                self.print(f'Warning: JudgeLine{line.id}.Event[0].startTime != 0')
+                flag = 0
+            elif i > 0:
+                if j.startTime != evt[i - 1].endTime:
+                    self.print(f'Warning: JudgeLine{line.id}.Event[{i}].startTime != JudgeLine{line.id}.Event[{i - 1}].endTime')
+                    flag = 0
+            elif i == len(evt) - 1 and j.endTime <= 1e7:
+                self.print(f'Warning: JudgeLine{line.id}.Event[{i}].endTime <= 1e7')
+                flag = 0
+        return flag
+    def check_line(self):
+        self.print('Check speedEvent')
+        flag = 1
+        for i in self.lines:
+            if not self.check_line_event(i, i.speedEvents, True):
+                flag = 0
+        if flag:
+            self.print('OK')
+        self.print('Check disappearEvent')
+        flag = 1
+        for i in self.lines:
+            if not self.check_line_event(i, i.disappearEvents):
+                flag = 0
+        if flag:
+            self.print('OK')
+        self.print('Check moveEvent')
+        flag = 1
+        for i in self.lines:
+            if not self.check_line_event(i, i.moveEvents):
+                flag = 0
+        if flag:
+            self.print('OK')
+        self.print('Check rotateEvent')
+        flag = 1
+        for i in self.lines:
+            if not self.check_line_event(i, i.rotateEvents):
+                flag = 0
+        if flag:
+            self.print('OK')
+    def notes(self):
+        cnt = 0
+        for i in self.lines:
+            cnt += i.notes()
+        self.notes = cnt
+        return self.notes
+    def highlight(self):
+        for i in self.lines:
+            for j in i.notesAbove:
+                j.find_near(self)
+            for j in i.notesBelow:
+                j.find_near(self)
+    def render(self, time, screen, options):
+        for i in self.lines:
+            i.render(time, screen, util.LINE_COLOR, self.fv, options)
+        # 后渲染界面，要不然会被挡
+        font = pygame.font.Font('font.ttf', 25)
+        combo_font = pygame.font.Font('font.ttf', 20)
+        combo_font2 = pygame.font.Font('font.ttf', 35)
+        name = font.render(self.name, False, util.TEXT_COLOR)
+        lvl = font.render(self.lvl, False, util.TEXT_COLOR)
+        width, height = screen.get_size()
+        screen.blit(name, (10, height - 30))
+        screen.blit(lvl, (width - lvl.get_width() - 10, height - 30))
+        self.jm.check(screen, time)
+        combo_num = combo_font2.render(str(self.jm.combo), False, util.TEXT_COLOR)
+        combo_text = combo_font.render('AUTOPLAY', False, util.TEXT_COLOR)
+        if self.jm.combo > 2:
+            screen.blit(combo_num, (width / 2 - combo_num.get_width() / 2, 0))
+            screen.blit(combo_text, (width / 2 - combo_text.get_width() / 2, 40))
+        score_num = combo_font2.render(str(round(self.jm.combo / self.notes * 1e6)).zfill(7), False, util.TEXT_COLOR)
+        screen.blit(score_num, (width - score_num.get_width() - 10, 10))
+def number_notes(line):
+    line.notesAbove.sort(key=lambda x: x.time)
+    line.notesBelow.sort(key=lambda x: x.time)
+    for i, j in enumerate(line.notesAbove):
+        j.id = i
+    for i, j in enumerate(line.notesBelow):
+        j.id = i
+    line.speedEvents.sort(key=lambda x: x.startTime)
+    line.disappearEvents.sort(key=lambda x: x.startTime)
+    line.moveEvents.sort(key=lambda x: x.startTime)
+    line.rotateEvents.sort(key=lambda x: x.startTime)
+    return line
---
+++ note.py
@@ -0,0 +1,88 @@
+import pygame
+import util
+class Note():
+    def __init__(self, noteJson, line, isAbove):
+        self.type = int(noteJson['type'])
+        self.time = float(noteJson['time'])
+        self.positionX = float(noteJson['positionX'])
+        self.holdTime = float(noteJson.get('holdTime', 0))
+        self.speed = float(noteJson['speed'])
+        self.floorPosition = float(noteJson['floorPosition'])
+        self.line = line
+        self.isAbove = isAbove
+
+        self.x = 0
+        self.y = 0
+        self.deg = 0
+        self.scored = 0
+        self.id = 0
+        self.hl = False
+        self.pos = []
+    def find_near(self, chart):
+        for i in chart.all_notes:
+            if util.eq(util.timeToSec(self.line.bpm, self.time), util.timeToSec(i.line.bpm, i.time), eps=0.02) and (self.id != i.id or self.line.id != i.line.id):
+                self.hl = i.hl = True
+    def render(self, screen, time, fv, options):
+        self.deg = -self.line.deg
+        if 'notescale' in options:
+            scale = float(options['notescale'])
+        else:
+            scale = 1
+        if self.type != 3:
+            yDist = self.speed * (self.floorPosition - self.line.floorPosition)
+            linePos = util.calcNotePos(self, yDist, fv)
+            self.pos = linePos
+            note(screen, *linePos, self, scale)
+            '''if 'showid' in options:
+                font = pygame.font.Font('font.ttf', 20)
+                id_text = font.render(str(self.id), False, (255, 255, 255))
+                screen.blit(id_text, linePos)'''   # 性能代价太大
+        else:
+            yDist = self.floorPosition - self.line.floorPosition
+            headPos = util.calcNotePos(self, max(0, yDist), fv)
+            if time <= self.time:
+                yDistEnd = yDist + self.speed * util.timeToSec(self.line.bpm, self.holdTime)
+            elif self.time < time <= self.time + self.holdTime:
+                yDistEnd = self.speed * util.timeToSec(self.line.bpm, self.time + self.holdTime - time)
+            else:
+                yDistEnd = 0
+            endPos = util.calcNotePos(self, yDistEnd, fv)
+            hold(screen, *headPos, *endPos, self, scale)
+            if time > self.time + self.holdTime and not self.scored:
+                self.scored = 1
+            if util.inrng(time, self.time, self.time + self.holdTime):
+                if not self.scored:
+                    hitPos = util.calcNotePos(self, 0, fv)
+                    hit(screen, *hitPos, self.deg)
+def note(screen, x, y, note, scale):
+    width, height = screen.get_size()
+    if not util.onScreen(x, y):
+        return
+    if note.line.id == 4:
+        print(note.id, note.speed)
+    left = util.rotate(x, y, x - 0.07 * height * scale, y, note.deg)
+    right = util.rotate(x, y, x + 0.07 * height * scale, y, note.deg)
+    color = [None, (10, 195, 255), (240, 237, 105), None, (254, 67, 101)]
+    pygame.draw.line(screen, color[note.type], left, right, int(0.01 * height))
+    if note.hl:
+        pygame.draw.circle(screen, util.LINE_COLOR, (int(x), int(y)), int(0.02 * height * scale))
+def hold(screen, headX, headY, endX, endY, note, scale):
+    width, height = screen.get_size()
+    if not util.onScreen(headX, headY):
+        return
+    '''scr = pygame.Surface((0.14 * height, math.sqrt((headX - endX) ** 2 + (headY - endY) ** 2)), pygame.SRCALPHA)
+    scr.fill((10, 195, 255))
+    midX = (headX + endX) / 2
+    midY = (headY + endY) / 2
+    rotated = pygame.transform.rotate(scr, note.deg)
+    rect = rotated.get_rect(center=(midX, midY))
+    screen.blit(rotated, rect)''' # 同上
+    pygame.draw.line(screen, (10, 195, 255), (headX, headY), (endX, endY), int(0.14 * height * scale))
+    if note.hl:
+        pygame.draw.circle(screen, util.LINE_COLOR, (int(headX), int(headY)), int(0.02 * height * scale))
+def hit(screen, x, y, deg):
+    scr = pygame.Surface((20, 20), pygame.SRCALPHA)
+    scr.fill(util.LINE_COLOR)
+    rotated = pygame.transform.rotate(scr, deg)
+    rect = rotated.get_rect(center=(x, y))
+    screen.blit(rotated, rect)
